2d8cd65e79f1250c1952ee3bf29a3e03188dec41
JoseHector1@gmail.com <JoseHector1@gmail.com@e7f208a6-c03e-4f5b-1994-08bba5d67660>
2012-12-19 19:18:11 +0000

    script de BBDD

---
+++ trunk/semillaWS/src/main/scripts/script.sql
@@ -0,0 +1,7 @@
+create table busines(id varchar(5),nombre varchar(50),direccion varchar(100),url varchar(100),telefono varchar(15),email varchar(40));
+
+create table person (id VARCHAR(10),nombre VARCHAR(20),apellido1 VARCHAR(20),apellido2 VARCHAR(20),busines VARCHAR(5));
+
+insert into person values('1','JOSE HECTOR','LOPEZ','BENAVENTE','1');
+insert into person values('2','ALBERTO','CASTILLO','LOPEZ','1');
+insert into person values('3','MARIO','DIAZ','','1');
